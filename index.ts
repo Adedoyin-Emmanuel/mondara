5556834cb5cd82d9c9029b4e2da82ce4d9a30552
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-04-29 14:30:31 +0100

    chore: add assets

--- index.ts
+++ index.ts
@@ -1,5 +1,19 @@
+import fs from "fs";
+import { v4 as uuidv4 } from "uuid";
 import { FimidaraEndpoints } from "fimidara";
 
 const fimidara = new FimidaraEndpoints({
   authToken: process.env.FIMIDARA_AUTH_TOKEN as string,
 });
+
+
+/**
+ * 
+ * Let's me create a file. I'm not sure you can create a file without creating a folder first.
+ * But then, let's try.
+ */
+
+const file = await fimidara.files.uploadFile({
+    fileId: uuidv4(),
+    
+})
