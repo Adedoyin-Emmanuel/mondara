9250460055d99ff8f30ecc4290289ca9e121f614
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-04-29 16:02:24 +0100

    refactor: update file handling logic and remove unused image asset

--- index.ts
+++ index.ts
@@ -6,14 +6,32 @@ const fimidara = new FimidaraEndpoints({
   authToken: process.env.FIMIDARA_AUTH_TOKEN as string,
 });
 
-
 /**
- * 
+ *
  * Let's me create a file. I'm not sure you can create a file without creating a folder first.
- * But then, let's try.
+ * But then, let's try. Lol It isn't possible. Let's create a folder first then.
  */
 
-const file = await fimidara.files.uploadFile({
-    fileId: uuidv4(),
-    
-})
+// const folder = await fimidara.folders.addFolder({
+//   description: "This is a test folder",
+//   folderpath: "temi",
+// });
+
+//console.log(JSON.stringify(folder, null, 2));
+
+// const workspaceRootname = "temi";
+
+// const file = await fimidara.files.uploadFile({
+//   data: fs.readFileSync("./assets/emma.jpg"),
+//   size: fs.statSync("./assets/emma.jpg").size,
+//   filepath: `${workspaceRootname}/Temi`,
+//   description: "A picture of Emma",
+// });
+
+// console.log(JSON.stringify(file, null, 2));
+
+const files = await fimidara.files.getFileDetails({
+  fileId: "file000_pFD1-usV5KmEhn9QDlj1I",
+});
+
+console.log(JSON.stringify(files, null, 2));
